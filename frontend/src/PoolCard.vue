49cad287f17556873ce4ac1ba58317aa64567969
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-20 07:54:30 -0600

    Prep for squares

--- frontend/src/PoolCard.vue
+++ frontend/src/PoolCard.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card-pf card-pf-view">
             <div class="card-pf-body">
                 <div class="data-row">
                     <h2 class="card-pf-title pool-name">{{ pool.name }}</h2>
                     <div class="pool-type">{{ pool.type.toUpperCase() }}</div>
                 </div>
-    
+                <usage-map :used="pool.inUse" :max="pool.maxSize" :largest="greatestSize" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import UsageMap from './UsageMap.vue';
+
 export default {
     props: {
         pool: Object,
         greatestSize: Number
+    },
+    components: {
+        'usage-map': UsageMap
     }
 }
 </script>
---
+++ frontend/src/UsageMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="usage-map">{{ used }} + {{ avail }} ({{ empty }})</div>
+</template>
+
+<script>
+export default {
+    props: {
+        used: Number,
+        max: Number,
+        largest: Number
+    },
+    computed: {
+        avail() {
+            return this.max - this.used;
+        },
+        empty() {
+            return this.largest - this.max;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.usage-map {
+    width: 100%;
+}
+</style>
