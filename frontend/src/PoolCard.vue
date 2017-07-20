a7f138141f499db19ab68955e019a30c5132fa5a
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-20 08:51:01 -0600

    Separators

--- frontend/src/PoolCard.vue
+++ frontend/src/PoolCard.vue
@@ -6,6 +6,7 @@
                     <h2 class="card-pf-title pool-name">{{ pool.name }}</h2>
                     <div class="pool-type">{{ pool.type.toUpperCase() }}</div>
                 </div>
+                <div class="pf-card-separator" />    
                 <usage-map :used="pool.inUse" :max="pool.maxSize" :largest="greatestSize" :queued="pool.queueSize" />
                 <div class="pf-card-separator" />
     
@@ -60,6 +61,6 @@ export default {
 .card-pf-body .pf-card-separator {
     height: 1px;
     background: #e1e1e1;
-    margin: 15px 0;    
+    margin: 15px 0;
 }
 </style>
