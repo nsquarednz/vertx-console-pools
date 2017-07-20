8fee0fce98cff9c0ba8a11d2d3e8155c5023d6db
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-20 08:12:26 -0600

    Add different colors

--- frontend/src/UsageMap.vue
+++ frontend/src/UsageMap.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="usage-map">
-        <div class="square used" v-for="n in used" :key="n" /><div class="square avail" v-for="n in avail" :key="n" /><div class="square empty" v-for="n in empty" :key="n" />
+        <div class="square" :class="usedSquareClass" v-for="n in used" :key="n" /><div class="square avail" v-for="n in avail" :key="n" /><div class="square empty" v-for="n in empty" :key="n" />
     </div>
 </template>
 
@@ -17,6 +17,16 @@ export default {
         },
         empty() {
             return this.largest - this.max;
+        },
+        usedSquareClass() {
+            const ratio = this.used / this.max;
+            if (ratio > 0.6) {
+                return 'used-hot';
+            } else if (ratio > 0.33) {
+                return 'used-warm';
+            } else {
+                return 'used';
+            }
         }
     }
 }
@@ -40,11 +50,19 @@ export default {
     background: #f9d67a;
 }
 
+.used-warm {
+    background: #f39c3d;
+}
+
+.used-hot {
+    background: #cd0000;
+}
+
 .avail {
     background: #bee1f4;
 }
 
 .empty {
-    background: #e1e1e1;
+    background: #f1f1f1;
 }
 </style>
