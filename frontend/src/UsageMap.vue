341033bd5f6f9519179fe9369485775659016076
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-20 08:06:25 -0600

    Newlines break the layout

--- frontend/src/UsageMap.vue
+++ frontend/src/UsageMap.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="usage-map">{{ used }} + {{ avail }} ({{ empty }})</div>
+    <div class="usage-map">
+        <div class="square used" v-for="n in used" :key="n" /><div class="square avail" v-for="n in avail" :key="n" /><div class="square empty" v-for="n in empty" :key="n" />
+    </div>
 </template>
 
 <script>
@@ -23,5 +25,26 @@ export default {
 <style lang="scss" scoped>
 .usage-map {
     width: 100%;
+    margin: 10px 0;
+    line-height: 12px;
+}
+
+.square {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    margin-right: 2px;
+}
+
+.used {
+    background: #f9d67a;
+}
+
+.avail {
+    background: #bee1f4;
+}
+
+.empty {
+    background: #e1e1e1;
 }
 </style>
